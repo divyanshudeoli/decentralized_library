9abd40ae140344c0e6587e195ee6f0e85940ed74
Divyanshu Deoli <60615629+divyanshudeoli@users.noreply.github.com>
2021-04-23 11:32:48 +0530

    Login form with swing

--- block.java
+++ block.java
@@ -1,14 +1,33 @@
-public class Block {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException; 
+
+public class block {
     private String hash;
     private String previousHash;
     private String data;
     private long timeStamp;
-    private int nonce;
- 
-    public Block(String data, String previousHash, long timeStamp) {
+ 	
+    String calculateBlockHash(){
+      String message = data+previousHash+Long.toString(timeStamp);
+      try{
+      	MessageDigest md = MessageDigest.getInstance("SHA-256");
+      	md.update(message.getBytes());
+      	}
+      catch(NoSuchAlgorithmException ex){
+      	System.out.println(ex);
+      }
+      byte[] bytes= md.digest(); 
+      StringBuffer buffer = new StringBuffer();
+      for(byte b : bytes) {
+        buffer.append(String.format("%02x", b));
+      }
+      return buffer.toString();
+    }
+
+    public block(String data, String previousHash, long timeStamp) {
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = timeStamp;
-        this.hash = calculateBlockHash();
+        calculateBlockHash();
     }
 }
